90ef9a65f0eeba439a8a8ac3d3f5c33955323fcd
shivammaheshwari9837 <shivammaheshwari9837@gmail.com>
2023-05-22 23:20:18 +0530

    Integrated API Call

---
+++ NewApp/APICaller.swift
@@ -0,0 +1,59 @@
+//
+//  APICaller.swift
+//  NewApp
+//
+//  Created by Shivam Maheshwari on 22/05/23.
+//
+
+import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constants {
+        static let topHeadlinesURL = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=ea56a3032e394680a00a06401b96dbdd")
+    }
+    
+    private init() {}
+    
+    public func getTopStories(completion: @escaping (Result<[APIResponse], Error>) -> Void) {
+        guard let url = Constants.topHeadlinesURL else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data {
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+}
+
+//Models
+
+struct APIResponse: Codable {
+    let articles: [Article]?
+}
+
+struct Article: Codable {
+    let source: Source?
+    let title: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+}
+
+struct Source: Codable {
+    let name: String?
+}
--- NewApp/ViewController.swift
+++ NewApp/ViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "News"
+        view.backgroundColor = .systemBackground
+        APICaller.shared.getTopStories { result in
+            
+        }
         // Do any additional setup after loading the view.
     }
 
